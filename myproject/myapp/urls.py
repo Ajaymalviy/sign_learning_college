1851c256dcde8493c09a414c0400a4fb5c5b993c
t <ajeymalviya143@gmail.com>
2024-12-01 17:42:47 +0530

    added new url for logout

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_views, name='login'), 
     path('signup/', views.signup_view, name='signup'), 
+    path('logout/', views.logout, name='logout'), 
+    path('speech-to-text/', views.speech_to_text, name='speech_to_text'),
+
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
