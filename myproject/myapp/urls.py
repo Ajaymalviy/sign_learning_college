0075e98f10be3b8a6890ab4a27a59fd1941dbf64
t <ajeymalviya143@gmail.com>
2024-12-06 19:34:08 +0530

    added new urls in urls file of project

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -8,6 +8,11 @@ urlpatterns = [
     path('login/', views.login_views, name='login'), 
     path('signup/', views.signup_view, name='signup'), 
     path('logout/', views.logout, name='logout'), 
-    # path('speech-to-text/', views.speech_to_text, name='speech_to_text'),
+    path('speech_to_text/', views.speech_to_text, name='speech_to_text'),
+    # path('gif-videocheck/', views.gif_display, name='gif_display'),
+    path('tryyy/', views.tryyy, name='tryyy'),
+    path('text_to_sign/', views.text_to_sign, name='text_to_sign'),
+    path('videocheck/', views.videocheck, name='videocheck'),
+
 
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
